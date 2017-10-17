d47411b9099ad42c555f083e59573c03d8964ce0
kevin tsou <jam10636@gmail.com>
2017-10-17 23:24:50 +0000

    Added adding function

---
+++ app.js
@@ -0,0 +1,19 @@
+augular.module('contacts', []);
+app.controller('ContactsController', mainCtrl);
+
+function mainCtrl($scope)
+{
+    $scope.usercontacts = []
+    $scope.addNew = function (user) {
+        
+        $scope.usercontacts.push({
+                          name: user.name,
+                          photo: user.url,
+                          address: user.address,
+                          phone: user.phone,
+                          }); /* [1] */
+        user.name="";
+        user.url="";
+        user.address="";
+        user.phone="";  
+};
