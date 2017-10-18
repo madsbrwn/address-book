65d86190623e5800a5ac8c7ea5623a6990c0203c
madelinedickson <dickson.maddy@gmail.com>
2017-10-18 14:35:28 -0600

    display angular stuff

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-augular.module('contacts', []);
-app.controller('ContactsController', mainCtrl);
+angular.module('contacts', [])
+.controller('ContactsController', mainCtrl)
+.directive ('user', contactsDirective);
 
 function mainCtrl($scope)
 {
     $scope.usercontacts = []
-    $scope.addNew = function (user) {
-        
+    $scope.addNew = function (contact) {
+
         $scope.usercontacts.push({
-                          name: user.name,
-                          photo: user.url,
-                          address: user.address,
-                          phone: user.phone
-                          }); /* [1] */
-        user.name="";
-        user.url="";
-        user.address="";
-        user.phone="";  
-};
+                          name: contact.name,
+                          url: contact.url,
+                          phone: contact.phone,
+                          email: contact.email,
+                          address: contact.address,
+                          });
+        contact.name="";
+        contact.url="";
+        contact.phone="";
+        contact.email="";
+        contact.address="";
+    };
+}
+
+  function contactsDirective () {
+    return {
+      scope: {
+        contact: '='
+      },
+      restrict: 'E',
+      replace: 'true',
+      template: (
+        '<div class="contact">' +
+          '<span class="contactTitle">' +
+            '<img ng-src="{{contact.url}}" />' +
+          '</span>' +
+          '<span class="contactTitle">' +
+            '<h4>{{contact.name}}</h4>' +
+          '</span>' +
+          '{{contact.phone}}<br>' +
+          '<a href="mailto:{{contact.email}}">{{contact.email}}</a><br>' +
+          '{{contact.address}}' +
+          //something to delete
+        '</div>'
+      ),
+      link: link
+    };
+  }
+
+  function link (scope) {
+  if (!scope.contact.url) {
+    scope.contact.url = 'https://i.imgflip.com/4a8he.jpg';
+  }
+}
